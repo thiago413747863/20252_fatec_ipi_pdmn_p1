0e61a0cd3aaa1e23acf890d39c7d4ce8ef337506
Thiago Santos <thiago.santana8@fatec.sp.gov.br>
2025-09-21 16:36:01 -0300

    feat(p1): cria componente principal e faz hello

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+    return (
+      <main className="container my-5">
+        <div className='row'>
+          <p className='col-12'>Hello, Lembretes</p>
+        </div>
+      </main>
+    );
+  } 
+}
+
+export default App;
